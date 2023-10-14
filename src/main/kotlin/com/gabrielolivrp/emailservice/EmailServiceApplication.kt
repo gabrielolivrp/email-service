762b9aff0f99e9f323652a1a4c969adf761956b7
Gabriel Augusto <gabrielolivrp@gmail.com>
2023-10-14 12:10:09 -0300

    first commit

---
+++ src/main/kotlin/com/gabrielolivrp/emailservice/EmailServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.gabrielolivrp.emailservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmailServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmailServiceApplication>(*args)
+}
